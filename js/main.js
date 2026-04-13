7b0192c5fe6474d1756b3c7b76786118b4437154
ColinFrey <Acefrey2020@gmail.com>
2026-04-13 18:13:47 -0500

    added photos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-// JS scripts placed here
+window.addEventListener('load', () => {
+    const items = document.querySelectorAll('.masonry-item');
+    
+    items.forEach((item, index) => {
+        
+        item.style.opacity = '0';
+        item.style.transform = 'translateY(20px)';
+        item.style.transition = 'all 0.6s ease-out';
+
+        setTimeout(() => {
+            item.style.opacity = '1';
+            item.style.transform = 'translateY(0)';
+        }, 100 * index);
+    });
+
+    console.log("Portfolio loaded and animated!");
+});
